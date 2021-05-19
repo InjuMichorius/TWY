26410a317fc7389ac430d3640b83a06fb9d2ea67
HappyPantss <sergioeijben@hotmail.com>
2021-05-19 18:04:44 +0200

    :sparkles: Added server side routing and beginning of styling

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const path = require('path')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static('src/public'));
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src/public/views'))
+
+app.get('/', function(req, res) {
+    res.render("index")
+});
+
+server.listen(port, () => {
+    console.log('listening on port ', 'http://localhost:' + port)
+})

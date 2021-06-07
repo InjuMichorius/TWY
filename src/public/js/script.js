8ba3fc61c2a2edb9a66a53d7ea896df444af4270
HappyPantss <sergioeijben@hotmail.com>
2021-06-07 12:35:00 +0200

    :lipstick: changing the style

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,17 +1,17 @@
-let messages = document.querySelector('ul');
-let gameForm = document.querySelector('#gameForm');
-let input = document.querySelector('#textInput');
-const voice = document.querySelector(".btnVoice");
-const textInput = document.querySelector("#textInput");
-const send = document.querySelector(".btnAnswer");
+let messages = document.querySelector('.chat_messages')
+let gameForm = document.querySelector('#gameForm')
+let input = document.querySelector('#textInput')
+const voice = document.querySelector(".btnVoice")
+const textInput = document.querySelector("#textInput")
+const send = document.querySelector(".btnAnswer")
 
 var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-var recorder = new SpeechRecognition();
+var recorder = new SpeechRecognition()
 
 gameForm.addEventListener('submit', (event) => {
     event.preventDefault()
     if (input.value) {
-        sendMessage();
+        sendMessage()
         input.value = ''
     }
 })
@@ -61,30 +61,30 @@ recorder.onstart = () => {
 recorder.onresult = (event) => {
     const resultIndex = event.resultIndex;
     const transcript = event.results[resultIndex][0].transcript
-    textInput.value = transcript;
-    console.log(transcript);
+    textInput.value = transcript
+    console.log(transcript)
 };
 
 voice.addEventListener('click', () => {
     console.log('click')
-    recorder.start();
+    recorder.start()
 });
 
 send.addEventListener('click', () => {
     // console.log(inputField.value)   
     if (textInput.value) {
-        const fanMessage = document.createElement("li");
-        const meta = document.createElement("p");
-        const text = document.createElement("p");
-        fanMessage.classList.add("message");
-        fanMessage.classList.add("you");
+        const fanMessage = document.createElement("li")
+        const meta = document.createElement("p")
+        const text = document.createElement("p")
+        fanMessage.classList.add("message")
+        fanMessage.classList.add("you")
         meta.classList.add("text_meta")
-        meta.innerText = "You";
-        text.classList.add("text");
-        text.innerText = textInput.value;
-        fanMessage.appendChild(meta);
-        fanMessage.appendChild(text);
-        messages.appendChild(fanMessage);
-        textInput.value = "";
+        meta.innerText = "You"
+        text.classList.add("text")
+        text.innerText = textInput.value
+        fanMessage.appendChild(meta)
+        fanMessage.appendChild(text)
+        messages.appendChild(fanMessage)
+        textInput.value = ""
     }
 });

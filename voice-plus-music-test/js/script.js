51b95b3907fdc12c4eaae05d560bb404c70949e8
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-06-08 16:46:35 +0200

    tone.js is able to play a melody

--- voice-plus-music-test/js/script.js
+++ voice-plus-music-test/js/script.js
@@ -1,4 +1,4 @@
-// const audioCtx = new AudioContext();
+// import * as Tone from '/node_modules/tone/build/Tone.js';
 
 const speechButton = document.getElementById('talk');
 let ul = document.querySelector('ul');
@@ -8,6 +8,11 @@ let text;
 var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
 recognition.interimResults = true;
+
+let sequence1, simpSynth;
+let bgMelody = ["C3", ["E3", "G3"], "D3", ["C3", "A3"], "B2", "C2", ["E3", "A2"], "G2", "C4"];
+let injuMelody = ["E3", ["D3", "E3", "F3", "E3"], "B4", ["B2", "A2"], "A3", "B3", "C3", "B2"];
+let injuMelodyTwo = ["B3", ["G3", "A3"], "E3", null, "E3", "G3", "A3", "A3", "B3"];
 
 let bpm = 150;
 let kickBeat = null
@@ -32,10 +37,10 @@ const snarePatterns = [
 ]
 
 const randomKick = kickPatterns[Math.floor(Math.random() * kickPatterns.length)];
-console.log(randomKick);
+// console.log(randomKick);
 
 const randomSnare = snarePatterns[Math.floor(Math.random() * snarePatterns.length)];
-console.log(randomSnare);
+// console.log(randomSnare);
 
 let underEight = Math.floor(Math.random() * 8) + 1;
 let underThirteen = Math.floor(Math.random() * 13) + 1;
@@ -54,6 +59,75 @@ recognition.addEventListener('result', (e) => {
         newListItem = document.createElement('li')
     }
 })
+
+recognition.addEventListener('end', (e) => {
+    let lowerText = text.toLowerCase()
+
+    if (lowerText.search('random') >= 0 || lowerText.search('doe maar wat') >= 0 || lowerText.search('geef me een beat') >= 0) {
+        let newListItem = document.createElement('li'); // create li
+        let text = document.createTextNode(`Wat vind je van een ${lowerText}? Laten we verder gaan met een snare! Wat voor snare wil je?`); // create text node
+
+        createInstruments();
+
+        newListItem.appendChild(text); // append text node to li node
+        ul.appendChild(newListItem); // append li node to list
+
+    } else if (lowerText.search('langzaam') >= 0 || lowerText.search('langzamer') >= 0 || lowerText.search('rustiger') >= 0) {
+        let newListItem = document.createElement('li'); // create li
+        let text = document.createTextNode(`Oke je wilt het dus ${lowerText}. Komt voor elkaar!`); // create text node
+
+        bpm = bpm - 30;
+        console.log(bpm)
+        createInstruments();
+
+        newListItem.appendChild(text); // append text node to li node
+        ul.appendChild(newListItem); // append li node to list
+    } else if (lowerText.search('snel') >= 0 || lowerText.search('sneller') >= 0 || lowerText.search('faster') >= 0) {
+        let newListItem = document.createElement('li'); // create li
+        let text = document.createTextNode(`Oke je wilt het dus ${lowerText}. Komt voor elkaar!`); // create text node
+
+        bpm = bpm + 30;
+        console.log(bpm)
+        createInstruments();
+
+        newListItem.appendChild(text); // append text node to li node
+        ul.appendChild(newListItem); // append li node to list
+    } else {
+        unrecognized()
+    }
+})
+
+
+
+pause.addEventListener('click', pauseButton)
+play.addEventListener('click', playButton)
+speechButton.addEventListener('click', talk)
+synthBtn.addEventListener('click', async () => {
+    //     const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    //     const now = Tone.now()
+    //     synth.triggerAttack("D4", now);
+    //     // synth.triggerRelease("D4", now + 0.5);
+    //     synth.triggerAttack("F4", now + 0.5);
+    //     // synth.triggerRelease("F4", now + 1);
+    //     synth.triggerAttack("A4", now + 1);
+    //     // synth.triggerRelease("A4", now + 1.5);
+    //     synth.triggerAttack("C5", now + 1.5);
+    //     // synth.triggerRelease("C5", now + 2);
+    //     synth.triggerAttack("E5", now + 2);
+    //     // synth.triggerRelease("E5", now + 2.5);
+    //     Tone.Transport.start()
+    //     Tone.Transport.bpm.value = 100;
+    // Tone.start();
+    // sequence1.start();
+    setup();
+});
+
+function unrecognized() {
+    let newListItem = document.createElement('li')
+    let text = document.createTextNode("I have no idea what you're talking about")
+    newListItem.appendChild(text)
+    ul.appendChild(newListItem)
+}
 
 function createInstruments() {
     blip.sampleLoader()
@@ -104,54 +178,6 @@ function createInstruments() {
     }
 }
 
-recognition.addEventListener('end', (e) => {
-    let lowerText = text.toLowerCase()
-
-    if (lowerText.search('random') >= 0 || lowerText.search('doe maar wat') >= 0) {
-        let newListItem = document.createElement('li'); // create li
-        let text = document.createTextNode(`Wat vind je van een ${lowerText}? Laten we verder gaan met een snare! Wat voor snare wil je?`); // create text node
-
-        createInstruments();
-
-        newListItem.appendChild(text); // append text node to li node
-        ul.appendChild(newListItem); // append li node to list
-
-    } else if (lowerText.search('langzaam') >= 0 || lowerText.search('langzamer') >= 0 || lowerText.search('rustiger') >= 0) {
-        let newListItem = document.createElement('li'); // create li
-        let text = document.createTextNode(`Oke je wilt het dus ${lowerText}. Komt voor elkaar!`); // create text node
-
-        bpm = bpm - 30;
-        console.log(bpm)
-        createInstruments();
-
-        newListItem.appendChild(text); // append text node to li node
-        ul.appendChild(newListItem); // append li node to list
-    } else if (lowerText.search('snel') >= 0 || lowerText.search('sneller') >= 0 || lowerText.search('faster') >= 0) {
-        let newListItem = document.createElement('li'); // create li
-        let text = document.createTextNode(`Oke je wilt het dus ${lowerText}. Komt voor elkaar!`); // create text node
-
-        bpm = bpm + 30;
-        console.log(bpm)
-        createInstruments();
-
-        newListItem.appendChild(text); // append text node to li node
-        ul.appendChild(newListItem); // append li node to list
-    } else {
-        unrecognized()
-    }
-})
-
-function unrecognized() {
-    let newListItem = document.createElement('li')
-    let text = document.createTextNode("I have no idea what you're talking about")
-    newListItem.appendChild(text)
-    ul.appendChild(newListItem)
-}
-
-pause.addEventListener('click', pauseButton)
-play.addEventListener('click', playButton)
-speechButton.addEventListener('click', talk)
-
 function stopAudio() {
     if (kickBeat !== null) {
         kickBeat.stop()
@@ -189,4 +215,30 @@ function playButton() {
     console.log('Hello world')
     play.style.display = "none"
     pause.style.display = "inline"
+}
+
+function setup() {
+    //Make the synth like normal
+    simpSynth = new Tone.Synth({
+        oscillator: {
+            volume: 0.1,
+            type: "fatsawtooth"
+        },
+        envelope: {
+            attack: 0.05,
+            decay: 0.5,
+            sustain: 0.5,
+            release: 1
+        }
+    }).toDestination(); //sends the synth's output to the speakers
+
+    //Here we generate the sequencer.
+    sequence1 = new Tone.Sequence(function (time, note) { //the object has a built in function where we call triggerAttackRelease(), which is what makes our synthesizer generate sound
+        simpSynth.triggerAttackRelease(note, 0.5);
+        //adding the console log lets the programmer see the current note being generated
+        console.log(note);
+    }, injuMelodyTwo, '4n');
+    Tone.Transport.bpm.value = 150; //how many beats(quarter notes) per minute
+    Tone.Transport.start(); //starts the transport
+    sequence1.start();
 }

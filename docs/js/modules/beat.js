5a7dd1f8ffd30095a8aa0918f47b035426ac8b30
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-07-05 12:19:51 +0200

    code cleanup, added json file support and added code comments everywhere

--- docs/js/modules/beat.js
+++ docs/js/modules/beat.js
@@ -1,29 +1,33 @@
+// kickBeat, snareBeat and hihatBeat have to be globaly declared because they have to be started or pause seperately from the beat setup
 let kickBeat = null
 let snareBeat = null
 let hihatBeat = null
-const lighting = document.querySelector('.twy-image')
-const blade = document.querySelector('.blade')
 
+// This function sets up the beat and takes in the radomly generated numbers and the randomly generated drum patterns as arrays and lastly it also takes in the beats per m inute
 export function setupBeat(randomEight, randomThirteen, randomFive, randomKickPattern, randomSnarePattern, beatsPerMinute) {
+    const lighting = document.querySelector('.twy-image')
+    const blade = document.querySelector('.blade')
+
+    // The sample loader uses the library Blip to load in the samples and create the loops
+    // Source: https://jshanley.github.io/blip/
     blip.sampleLoader()
+        // Loads in the samples. It only loads the samples which correspond with the randomly generated numbers. 
         .samples({
             'kick': `./sounds/kick${randomEight}.wav`,
             'snare': `./sounds/snare${randomThirteen}.wav`,
             'hihat': `./sounds/hihat${randomFive}.wav`,
         })
         .done(loaded)
-        .load();
+        .load()
 
+    // Once the samples are loaded they are able to be used in the loaded function
     function loaded() {
-        lighting.classList.add('default')
-        blade.classList.add('defaultBlade')
+        // Variables have to be created for each instrument and be linked to a sample which was previously declared
+        const kick = blip.clip().sample('kick')
+        const snare = blip.clip().sample('snare')
+        const hihat = blip.clip().sample('hihat')
 
-        // console.log("Samples are now loaded")
-
-        var kick = blip.clip().sample('kick')
-        var snare = blip.clip().sample('snare')
-        var hihat = blip.clip().sample('hihat')
-
+        // Creating a loop for the kick which takes the beats per minute for the right speed and it takes the randomly generated kick pattern
         kickBeat = blip.loop()
             .tempo(beatsPerMinute)
             .data(randomKickPattern)
@@ -31,8 +35,9 @@ export function setupBeat(randomEight, randomThirteen, randomFive, randomKickPat
                 if (d) {
                     kick.play(t)
                 }
-            });
+            })
 
+        // Creating a loop for the snaredrum which takes the beats per minute for the right speed and it takes the randomly generated snare pattern
         snareBeat = blip.loop()
             .tempo(beatsPerMinute)
             .data(randomSnarePattern)
@@ -40,21 +45,27 @@ export function setupBeat(randomEight, randomThirteen, randomFive, randomKickPat
                 if (d) {
                     snare.play(t)
                 }
-            });
+            })
 
+        // Creating a loop for the hihat which only takes the beats per minute for the right speed
         hihatBeat = blip.loop()
             .tempo(beatsPerMinute)
             .tick(function (t) {
                 hihat.play(t)
-            });
+            })
 
+        // Starts all the loops at once to make them play together
         kickBeat.start()
         snareBeat.start()
         hihatBeat.start()
 
+        // Adds a class to the lighting effect on the page. This will trigger the right animation in CSS
+        lighting.classList.add('default')
+        blade.classList.add('defaultBlade')
     }
 }
 
+// This function checks if the beats are not null which will stop / pause them
 export function stopBeat() {
     if (kickBeat !== null) {
         kickBeat.stop()
@@ -67,6 +78,7 @@ export function stopBeat() {
     }
 }
 
+// This function checks if not the beats are not null which will start / unpause them
 export function startBeat() {
     if (!kickBeat !== null) {
         kickBeat.start()
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,83 +1,48 @@
+// Importing functions from different modules
+import { randomKick, randomSnare, underEight, underThirteen, underFive, randomMelody } from './modules/randomize.js'
 import { setupBeat, stopBeat, startBeat } from './modules/beat.js'
 import { setupMelody, stopMelody, startMelody } from './modules/melody.js'
 import { setupEffect } from './modules/effects.js'
 
+// DOM elements needed globaly
 const speechButton = document.getElementById('talk')
 const chatBox = document.querySelector('.chat_messages')
 const playButton = document.getElementById('play')
 const pauseButton = document.getElementById('pause')
 const loading = document.querySelector('.loading')
+const lighting = document.querySelector('.twy-image')
+const blade = document.querySelector('.blade')
+
+// These let's have to be global and 'changable' because we only want one instance to exist and not declare them again
 let newMessage
 let userResponse
 let text
 
-let melodyPlayed = false;
-
-var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-var recognition = new SpeechRecognition()
+// These variable are needed for the Speech Recognition API
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
 recognition.interimResults = true
 
-const beat = ["beat", "doe maar wat", "random", "ritme", "slagwerk", "trommel", "kick", "kik", "snare", "hihat", "basedrum", "tom", "crash", "ride"]
-const melody = ["melodie", "melody", "lied", "deun", "loop", "tune", "wijs", "muziekstuk", "riedel", "klank", "compositie"]
-const slower = ["langzaam", "sloom", "slomer", "slow", "traag", "trager", "treuzelend", "zacht", "kalm", "rustig", "lui", "geleidelijk", "saai"]
-const faster = ["fast", "speed", "snel", "hard", "rap", "vlug", "vlot", "gauw", "subiet"]
-const effect = ["effect", "effekt", "perfect", "transitie", "gevolg", "lijp"]
-
-const checkIfExists = beat.concat(melody, slower, faster, effect)
-
-const lighting = document.querySelector('.twy-image')
-const blade = document.querySelector('.blade')
-
+// A global bpm is declared which will ensure the bpm is the same for the beat and the melody. This value will be changed so that's why it's a let
 let bpm = 150
 
-const kickPatterns = [
-    [0, 1, 0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0],
-    [1, 1, 0, 0, 1, 1, 0, 0],
-    [0, 1, 0, 0, 0, 1, 1, 0]
-]
+// This variable keeps track of if the melody has been played already. This value will be changed so that's why it's a let
+let melodyPlayed = false
 
-const snarePatterns = [
-    [0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 0, 0, 1],
-    [0, 0, 0, 1, 1, 0, 0, 1]
-]
-
-let randomKick = kickPatterns[Math.floor(Math.random() * kickPatterns.length)]
-let randomSnare = snarePatterns[Math.floor(Math.random() * snarePatterns.length)]
-
-let underEight = Math.floor(Math.random() * 8) + 1
-let underThirteen = Math.floor(Math.random() * 13) + 1
-let underFive = Math.floor(Math.random() * 5) + 1
-
-const melodies = [
-    ["C3", ["E3", "G3"], "D3", ["C3", "A3"], "B2", "C2", ["E3", "A2"], "G2", "C4"],
-    ["E3", ["D3", "E3", "F3", "E3"], "B4", ["B2", "A2"], "A3", "B3", "C3", "B2"],
-    ["B3", ["G3", "A3"], "E3", null, "E3", "G3", "A3", "A3", "B3"],
-    ["C#4", "D#4", "G#3", "D#4", "F4", ["G#4", "F#4", "F4"], "C#4", "D#4", "G#3", null, null],
-    ["C3", "D#3", "G3", "G#3", "D#3", "G#3", "A#3", ["G3", "G#3", "G3", "G#3", "G3"]],
-    ["C3", ["G3", "F#3", "G3", "F#3", "G3"], "D#3", "C3"],
-    [
-        ["C4", "B#3", "A3", "F3"], "G3", ["G3", "D4"], "C4", "B#3", "A3", ["A3", "A3"], "C4", ["B#3", "A3"], "G3", ["B#4", "A4", "B#4", "A4", "B#4"]
-    ]
-]
-
-let randomMelody = melodies[Math.floor(Math.random() * melodies.length)]
-
+// TWY sends you a welcome message
 window.addEventListener('load', () => {
-    const reply = "Hey jij daar! Mijn naam is TWY :) Ik hou van muziek maken en ik ben ontzettend benieuwd naar de muziek die jullie op aarde luisteren. Laten we beginnen met een beat, wat voor beat wil je?"
+    const reply = 'Hey you! My name is TWY :) I love making music, and I am very curious about the music you listen to on Earth. Let`s start with a beat, what kind of beat do you want?'
     twyResponseMessage(reply)
 })
 
 speechButton.addEventListener('click', () => {
-
+    // Start the recognition
     talk()
-    // console.log(checkIfExists)
-    // console.log('click')
     lighting.classList.remove('default')
     blade.classList.remove('defaultBlade')
 
-    pauseButton.disabled = false;
+    // Make sure the pause button is disabled because there is no music played yet
+    pauseButton.disabled = false
     pauseButton.classList.remove('inactive')
     playButton.classList.add('inactive')
 
@@ -95,34 +60,35 @@ speechButton.addEventListener('click', () => {
     newMessage.appendChild(userMeta)
     chatBox.appendChild(newMessage)
 
+    // Scrolls automatically to the last message
     chatBox.scrollTop = chatBox.scrollHeight
 })
 
+// Check if the recognition found any result
 recognition.addEventListener('result', (e) => {
     text = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
         .join('')
-
     userResponse.innerText = text
     newMessage.appendChild(userResponse)
-
     if (e.results[0].isFinal) {
         userResponse = document.createElement('p')
     }
-
     chatBox.scrollTop = chatBox.scrollHeight
 })
 
+// If recognition ended listening
 recognition.addEventListener('end', (e) => {
-    let lowerText = text.toLowerCase()
-
+    const lowerText = text.toLowerCase()
     compareData(lowerText)
 })
 
+// Event listeners to handle the play and pause actions
 playButton.addEventListener('click', play)
 pauseButton.addEventListener('click', pause)
 
+// This function handles the reply that TWY gives. It takes the response text for the message and then creates and appends all the DOM elements needed. It also has a delay of 1000ms for the reply to look more realistic
 function twyResponseMessage(response) {
     loading.classList.add('hidden')
     setTimeout(() => {
@@ -142,10 +108,10 @@ function twyResponseMessage(response) {
         chatBox.scrollTop = chatBox.scrollHeight
 
         loading.classList.remove('hidden')
-    }, 1000);
-
+    }, 1000)
 }
 
+// This function handles a specific reply. It still takes the response text and then creates and appends all the DOM elements needed. It still has a delay of 1000ms for the reply to look more realistic but this time it also creates a button that will refresh the page for you to make another song
 function twyResetConversation(response) {
     loading.classList.add('hidden')
     setTimeout(() => {
@@ -160,7 +126,7 @@ function twyResetConversation(response) {
         twyButton.classList.add('reset')
         twyMeta.innerText = 'TWY'
         twyResponse.innerText = response
-        twyButton.innerText = 'Opnieuw'
+        twyButton.innerText = 'Retry'
 
         twyMessage.appendChild(twyMeta)
         twyMessage.appendChild(twyResponse)
@@ -171,120 +137,147 @@ function twyResetConversation(response) {
 
         loading.classList.remove('hidden')
 
+        // Handles the button click for the reset button. It will refresh the page which will give the user another beat and melody
         const refreshButton = document.querySelector('.reset')
         refreshButton.addEventListener('click', () => {
             console.log('click')
             location.reload()
         })
-    }, 1000);
-
+    }, 1000)
 }
 
+// Function that checks if the speech matches certain words
 function compareData(speechToText) {
-    const existsToString = checkIfExists.toString()
+    let synonyms
 
-    // console.log(existsToString)
+    // Fetches the synonyms from the json file
+    fetch('./json/data.json')
+        .then(res => res.json())
+        // Puts the data into the synonyms variable
+        .then(data => synonyms = data)
+        // Puts the data into the function so it can check what the user said
+        .then(() => findSynonyms(synonyms))
 
-    if (speechToText.search(existsToString)) {
-        beat.filter((item) => {
-            if (speechToText.includes(item)) {
-                const reply = 'Wat vind je van deze sicke beat? Moet het tempo aangepast nog aangepast denk je? Anders gaan we verder met een melodie'
-                twyResponseMessage(reply)
+    // Handles the specific answers to the voice commands
+    function findSynonyms(synonymArray) {
+        // Converts the arrays into string so it can be searched by the first if statement
+        let stringedSynonyms = JSON.stringify(synonymArray)
+        if (speechToText.search(stringedSynonyms)) {
+            // Check if the question contains any synonym of the word 'beat'
+            synonymArray[0].beat.filter((item) => {
+                if (speechToText.includes(item)) {
+                    // The specific reply that will be 'sent' if you trigger the beat 
+                    const reply = 'What do you think of this sick beat? Do you think the tempo should be adjusted? Otherwise we will continue with a melody.'
+                    twyResponseMessage(reply)
 
-                setTimeout(() => {
-                    setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
-                }, 1500)
-            }
-        })
-        slower.filter((item) => {
-            if (speechToText.includes(item)) {
-                const reply = `Oke je wilt het dus langzamer. Komt voor elkaar!`
-                twyResponseMessage(reply)
+                    // Sets a delay and then it will play the beat
+                    setTimeout(() => {
+                        setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                    }, 1500)
+                }
+            })
+            // Checks if the question contains any synonym of the word 'slower'
+            synonymArray[0].slower.filter((item) => {
+                if (speechToText.includes(item)) {
+                    const reply = 'Okay so you want it slower. Consider it done!'
+                    twyResponseMessage(reply)
 
-                setTimeout(() => {
-                    bpm = bpm - 30
-                    setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                    setTimeout(() => {
+                        bpm = bpm - 30
+                        setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                        // Checks if the melody has been played already, if so it will also play the melody when the user says slower
+                        if (melodyPlayed == true) {
+                            setupMelody(randomMelody, bpm)
+                        }
+                    }, 1500)
+                }
+            })
+            // Checks if the question contains any synonym of the word 'faster'
+            synonymArray[0].faster.filter((item) => {
+                if (speechToText.includes(item)) {
+                    const reply = 'Okay so you want it faster. Consider it done!'
+                    twyResponseMessage(reply)
 
-                    if (melodyPlayed == true) {
+                    setTimeout(() => {
+                        bpm = bpm + 30
+                        setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                        // Checks if the melody has been played already, if so it will also play the melody when the user says faster
+                        if (melodyPlayed == true) {
+                            setupMelody(randomMelody, bpm)
+                        }
+                    }, 1500)
+                }
+            })
+            // Checks if the question contains any synonym of the word 'melody'
+            synonymArray[0].melody.filter((item) => {
+                if (speechToText.includes(item)) {
+                    const reply = 'Nice, let`s spice it up with more melody! How does this sound? Do you want to add an effect over it too?'
+                    twyResponseMessage(reply)
+
+                    setTimeout(() => {
+                        setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
                         setupMelody(randomMelody, bpm)
-                    }
-                }, 1500)
-            }
-        })
-        faster.filter((item) => {
-            if (speechToText.includes(item)) {
-                const reply = `Oke je wilt het dus sneller. Komt voor elkaar!`
-                twyResponseMessage(reply)
+                        melodyPlayed = true
+                    }, 1500)
+                }
+            })
+            // Checks if the question contains any synonym of the word 'effect'
+            synonymArray[0].effect.filter((item) => {
+                if (speechToText.includes(item)) {
+                    const reply = 'Now it really gets sick! The music may start to sound weird for now.'
+                    twyResponseMessage(reply)
 
-                setTimeout(() => {
-                    bpm = bpm + 30
-                    setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                    setTimeout(() => {
+                        setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
+                        // Checks if the melody has been played already, if so it will play the melody with the effect over it
+                        if (melodyPlayed == true) {
+                            setupMelody(randomMelody, bpm)
+                            setupEffect()
+                        }
+                    }, 1500)
 
-                    if (melodyPlayed == true) {
-                        setupMelody(randomMelody, bpm)
-                    }
-                }, 1500)
-            }
-        })
-        melody.filter((item) => {
-            if (speechToText.includes(item)) {
-                const reply = 'Nice, laten we het wat opfleuren met meer geluid! Wat dacht je hiervan?'
-                twyResponseMessage(reply)
-
-                setTimeout(() => {
-                    setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
-                    setupMelody(randomMelody, bpm)
-                    melodyPlayed = true
-                }, 1500)
-            }
-        })
-        effect.filter((item) => {
-            if (speechToText.includes(item)) {
-                const reply = 'Nu word het pas echt vet! De muziek kan raar gaan klinken'
-                twyResponseMessage(reply)
-
-                setTimeout(() => {
-                    setupBeat(underEight, underThirteen, underFive, randomKick, randomSnare, bpm)
-                    if (melodyPlayed == true) {
-                        setupMelody(randomMelody, bpm)
-                        setupEffect()
-                    }
-                }, 1500)
-
-                setTimeout(() => {
-                    const endmessage = 'Ik heb alles gedaan om een vette tune voor je te maken. Wil je er nog één maken, druk de knop'
-                    twyResetConversation(endmessage)
-                }, 2500)
-            }
-        })
-    } else if (checkIfExists.indexOf(speechToText) >= -1) {
-        console.log('HET WERKT NIET')
-        const reply = 'Huh ik snap niet wat je bedoelt :(. Misschien moet je wat anders zeggen'
-        twyResponseMessage(reply)
+                    setTimeout(() => {
+                        const endmessage = 'I did everything to make a cool tune for you. If you want to make another one, press the button.'
+                        twyResetConversation(endmessage)
+                    }, 2500)
+                }
+            })
+        } else if (synonymArray[0].indexOf(speechToText) >= -1) { // If the question doesn't contain a synonym, show an 'error'
+            const reply = 'I don`t understand what you mean :(. Maybe say something else.'
+            twyResponseMessage(reply)
+        }
     }
 }
 
+// When you click the microphone, to say something
 function talk() {
-    console.log("Speech enabled")
+    // Stop the beat and melody
     stopBeat()
     stopMelody()
+    // Recognition starts and listens to what you're saying
     recognition.start()
 }
 
+// When you click the pause button
 function pause() {
-    stopBeat()
-    stopMelody()
-    playButton.classList.remove('inactive')
-    pauseButton.classList.add('inactive')
     lighting.classList.remove('default')
     blade.classList.remove('defaultBlade')
+    // Stop the beat and melody
+    stopBeat()
+    stopMelody()
+    // Switch the pause button to start
+    playButton.classList.remove('inactive')
+    pauseButton.classList.add('inactive')
 }
 
+// When you click the start button
 function play() {
     lighting.classList.add('default')
     blade.classList.add('defaultBlade')
+    // Start the beat and melody
     startBeat()
     startMelody()
+    // Switch the start button to pause
     pauseButton.classList.remove('inactive')
     playButton.classList.add('inactive')
 }
